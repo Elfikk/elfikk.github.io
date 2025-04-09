23b4a4fc1e2f1d300582e9094b146c822f84961f
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-04-09 09:33:42 +0100

    Initial Shows Bodge List

---
+++ src/shows/ShowsBodge.js
@@ -0,0 +1,12 @@
+// Shows
+// Edinburgh Fringe 2022 - Stop the Press! - Show Designer/Operator. FLX S24/QLab4
+// Earthquakes in London - Assistant Light Designer / Light Operator - EOS
+// The Pillowman - Show Designer/Operator - EOS/QLab4
+// DramFest - Grinolio's Home - My only acting credit
+// October Show 2023 - Light Designer/DSM
+// Revue 2023 - Lead Light Designer/DSM
+// Romeo and Juliet - Lead Light Designer/DSM
+// Spring Awakening - Production Manager/Light Designer/Light Operator
+// Big Fish - Sound Operator - SD9
+// IDEA 2025 - Light Designer and Show Operator - Avo TT2/SD9
+// Should also mention a couple misc events nights i.e ACC Halloween 2023 (Light Op), CGCU Dinner (General Crew?), CSSA (Production Manager), Acapella Competition

d40c5b9eea6a31eb8571a588c2bcefea7dd5752d
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-04-30 01:20:44 +0100

    Assign Project Summary Grid and Make Transparent on Click

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -7,7 +7,7 @@ import {useState} from "react";
 function Projects() {
 
     // I'm starting to love Hooks.
-    const [opacityGrid, setGridOpacity] = useState(1);
+    const [inGridView, setInGridView] = useState(1);
     const [projID, setID] = useState(0);
 
     function expandProject(e) {
@@ -17,18 +17,38 @@ function Projects() {
 
         // I'd like a better transition; the clicker project should stay fully
         // visible, whilst the grid fades out and simultaneously the box gets larger.
-        setGridOpacity(0);
+        setInGridView(0);
+        updateBlurState(0.2);
+    }
+
+    function updateBlurState(blur)
+    {
+        const summaries = document.getElementsByClassName("project-holder");
+        for (let i = 0; i < summaries.length; i++)
+        {
+            summaries[i].style.opacity = blur;
+        }
     }
 
     function backToGrid(e) {
-        setGridOpacity(1);
+        setInGridView(0);
+        updateBlurState(1);
+    }
+
+    function projectSummaryRow(index) {
+        return (index / 3) | 0 + 1;
+    }
+
+    function projectSummaryCol(index) {
+        return index % 3;
     }
 
     const gridComponents = allProjs.map((project, index) =>
-        <ProjectSummary key = {index} projID={index} projDets={project} clickMethod={expandProject}/>
+        <ProjectSummary key = {index} projID={index} projDets={project} clickMethod={expandProject}
+        style = {{"grid-column": projectSummaryCol(index), "grid-row": projectSummaryRow(index)}}/>
     )
 
-    const gridMode =  <div id = "project-grid" style={{opacity: opacityGrid}}>{gridComponents}</div>
+    const gridMode =  <div id = "project-grid">{gridComponents}</div>
 
     return (
         <section id = "projects">
@@ -36,8 +56,10 @@ function Projects() {
             <p>Mostly personal, though I've highlighted a few academic ones. </p>
             <p>Click on a project for more detail, and click again to go back to the grid.</p>
 
-            {opacityGrid ? (
-                gridMode
+            {(gridMode)}
+
+            {inGridView ? (
+                <></>
             ) : (
                  <ProjectShowcase projDets={allProjs[projID]} clickMethod={backToGrid}/>
             )}

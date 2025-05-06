c48b67cc9f17e6a67288a3e4eb54e51ee5a35c07
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-05-06 23:00:31 +0100

    Add Hover Comment

--- src/shows/Shows.js
+++ src/shows/Shows.js
@@ -12,6 +12,7 @@ function Shows()
         <section id = "shows">
             <h1>Shows and Events</h1>
             <p>Mostly lighting unsurprisingly. I think I can be proud of the shows below given I had not done any of this prior to Edinburgh Fringe 2022, which itself was not a gentle introduction.</p>
+            <p>Hover over a show for more detail.</p>
             <div id = "showGrid">
                 {gridComponents}
             </div>

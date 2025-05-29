f43b3f01bd471249fd9ae842919696c45946d909
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-05-29 23:52:38 +0100

    Style Changes in Show for Mobile

--- src/shows/Shows.js
+++ src/shows/Shows.js
@@ -10,9 +10,11 @@ function Shows()
 
     return (
         <section id = "shows">
-            <h1>Shows and Events</h1>
-            <p>Mostly lighting unsurprisingly. I think I can be proud of the shows below given I had not done any of this prior to Edinburgh Fringe 2022, which itself was not a gentle introduction.</p>
-            <p>Hover over a show for more detail.</p>
+            <h1 id="showsHeader">Shows and Events</h1>
+            <div className="showsText">
+                <p>Mostly lighting unsurprisingly. I think I can be proud of the shows below given I had not done any of this prior to Edinburgh Fringe 2022, which itself was not a gentle introduction.</p>
+                <p>Hover over a show for more detail.</p>
+            </div>
             <div id = "showGrid">
                 {gridComponents}
             </div>
